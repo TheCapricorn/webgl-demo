44fc43f157572d898586c272fd68cb0da1491d69
Administrator <769053649@qq.com>
2019-05-23 23:24:51 +0800

    123

--- src/example/OrthoView.tsx
+++ src/example/OrthoView.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-const {useEffect,useRef,useState,Fragment} = React;
+const {useEffect,useRef,useState,useCallback,Fragment} = React;
 import {getWebGLContext,initShaders} from "lib/cuon-utils";
 import { Matrix4 } from '@/lib/cuon-matrix';
 const VSHADER_SOURCE = 
@@ -17,19 +17,18 @@ const FSHADER_SOURCE =
     'void main(){\n'+
         'gl_FragColor=v_Color;\n'+
     '}';
-let g_near =0.0;
-let g_far = 0.5;
+
 
 interface setObject {
-    setGNear:()=>void,
-    setGFar:()=>void,
-    setGL:()=>void,
-    setU_ProjMatrix:()=>void,
-    setProjMatrix:()=>void,
-    setVertex:()=>void,
+    setGNear:React.Dispatch<any>,
+    setGFar:React.Dispatch<any>,
+    setGL:React.Dispatch<any>,
+    setU_ProjMatrix:React.Dispatch<any>,
+    setProjMatrix:React.Dispatch<any>
+    setVertex:React.Dispatch<any>,
 }
 
-function main(canvasRef:React.RefObject<any>,obj:setObject):void{
+function main(canvasRef:React.RefObject<any>,obj:setObject){
     const gl:any = getWebGLContext(canvasRef.current,true);
 
     if(!gl){
@@ -51,31 +50,40 @@ function main(canvasRef:React.RefObject<any>,obj:setObject):void{
     const projMatrix = new Matrix4();
 
     document.onkeydown=function(ev){
-        keyDown(ev,gl,n,u_ProjMatrix,projMatrix);
+        keyDown(ev,gl,n,u_ProjMatrix,projMatrix,obj);
     }
-    draw(gl,n,u_ProjMatrix,projMatrix);
+    obj.setGL(gl);
+    obj.setVertex(n);
+    obj.setProjMatrix(projMatrix);
+    obj.setU_ProjMatrix(u_ProjMatrix);
+
+    return function(){
+        document.onkeydown=null;
+    }
 }
 
-function keyDown(ev:any, gl:any, n:number, u_ProjMatrix:any, projMatrix:any){
+
+const add = function(n:number):number{
+    return n+0.01;
+}
+
+const dec = function(n:number):number{
+    return n-0.01;
+}
+
+function keyDown(ev:any, gl:any, n:number, u_ProjMatrix:any, projMatrix:any,obj:setObject){
     switch(ev.keyCode){
-        case 39: g_near += 0.01; break; //right
-        case 37: g_near -= 0.01; break; //left
-        case 38: g_far += 0.01; break; //up
-        case 40: g_far -=0.01; break; //down
+        case 39: obj.setGNear(add) ; break; //right
+        case 37: obj.setGNear(dec) ;break //left
+        case 38: obj.setGFar(add); break; //up
+        case 40: obj.setGFar(dec); break; //down
         default: return;
     }
 
-    draw(gl,n,u_ProjMatrix,projMatrix);
+   
 }
 
-function draw( gl:any, n:number, u_ProjMatrix:any, projMatrix:any){
-     //设置视点和视线
-    projMatrix.setOrtho(-1,1,-1,1,g_near,g_far);
-    //将视图矩阵传递给u_ViewMatrix变量
-    gl.uniformMatrix4fv(u_ProjMatrix,false,projMatrix.elements);
-    gl.clear(gl.COlOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, n);
-}
+
 
 function initVertexBuffers(gl:any){
     const vertexColor = new Float32Array([
@@ -141,16 +149,35 @@ function initVertexBuffers(gl:any){
 
 const OrthoView =()=>{
     const canvasRef: React.RefObject<any>= useRef(null);
-    const [gl,setGL] = useState(null);
+    const [gl,setGL]:Array<any> = useState(null);
     const [vertex,setVertex] = useState(0);
     const [u_ProjMatrix,setU_ProjMatrix] = useState(null);
-    const [projMatrix,setProjMatrix] = useState(null);
+    const [projMatrix,setProjMatrix]:Array<any> = useState(null);
     const [g_near,setGNear] = useState(0.0);
     const [g_far,setGFar] = useState(0.5);
+    const draw = useCallback(function(projMatrix,g_near,g_far,u_ProjMatrix,gl,vertex){
+        //设置视点和视线
+        console.info(projMatrix)
+       projMatrix.setOrtho(-1,1,-1,1,g_near,g_far);
+       //将视图矩阵传递给u_ViewMatrix变量
+       gl.uniformMatrix4fv(u_ProjMatrix,false,projMatrix.elements);
+       gl.clear(gl.COlOR_BUFFER_BIT);
+       gl.drawArrays(gl.TRIANGLES, 0, vertex);
+   },[])
 
+    useEffect(()=>{ 
+        console.log('update',canvasRef.current);
+        return main(canvasRef,{setGNear,setGFar,setGL,setU_ProjMatrix,setProjMatrix,setVertex});
+    },[]);
+    
     useEffect(()=>{
-        main(canvasRef,{setGNear,setGFar,setGL,setU_ProjMatrix,setProjMatrix,setVertex});
-    },[])
+        console.log(g_near)
+        if(!projMatrix || !u_ProjMatrix || !vertex || !gl){
+            return;
+        }
+        
+        draw(projMatrix,g_near,g_far,u_ProjMatrix,gl,vertex)
+    },[projMatrix,g_near,g_far,u_ProjMatrix,gl,vertex])
 
     return (
         <Fragment>
